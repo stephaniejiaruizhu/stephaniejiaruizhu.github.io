c82eb805ffdb0560d35ff8e08f6792910b27be74
stephaniejiaruizhu <51218809+stephaniejiaruizhu@users.noreply.github.com>
2022-09-17 16:25:20 -0700

    Updates

---
+++ component---src-pages-footer-js-96a99d22992c25f37b4e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstephaniejiaruizhu=self.webpackChunkstephaniejiaruizhu||[]).push([[146],{5688:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=n(7294),a=n(7544),r=n(3168);var i=function(){return u.createElement(u.Fragment,null,u.createElement("p",{className:"footer-module--footer--m9gMX"}," Built with ",u.createElement(a.G,{icon:r.m6i})," in React"))}}}]);
+//# sourceMappingURL=component---src-pages-footer-js-96a99d22992c25f37b4e.js.map
