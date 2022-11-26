bb3f23dc427113a92fedf1f1aad40e9a1eee2e45
stephaniejiaruizhu <51218809+stephaniejiaruizhu@users.noreply.github.com>
2022-11-26 11:48:53 -0800

    lazy load images

--- src/pages/homeRight.js
+++ src/pages/homeRight.js
@@ -2,6 +2,7 @@ import React from "react";
 import * as rightStyles from "../styles/modules/right.module.less";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 function Right () {
     return(
@@ -15,11 +16,11 @@ function Right () {
             <p className={rightStyles.transition}> selected projects </p>
             <FontAwesomeIcon className={rightStyles.arrow} icon={faArrowDown}/>
             <div className={rightStyles.projectSection}>
-                <img className={rightStyles.cover} src={"https://res.cloudinary.com/dvjavf8xh/image/upload/v1668976726/misc/sfchronicle_mogiyl.png"} alt="various projects done at San Francisco Chronicle."/>
+                <LazyLoadImage className={rightStyles.cover} src={"https://res.cloudinary.com/dvjavf8xh/image/upload/v1668976726/misc/sfchronicle_mogiyl.png"} alt="various projects done at San Francisco Chronicle."/>
                 <p className={rightStyles.projectName}><a href="/sfchronicle">San Francisco Chronicle (digital design + web development) → </a></p>
             </div>
             <div className={rightStyles.projectSection}>
-                <img className={rightStyles.cover} src={"https://res.cloudinary.com/dvjavf8xh/image/upload/v1669175644/misc/shlcover_vacvge.jpg"} alt="mockups of a website done for Northwestern Feinberg's Student Health Leaders program."/>
+                <LazyLoadImage className={rightStyles.cover} src={"https://res.cloudinary.com/dvjavf8xh/image/upload/v1669175644/misc/shlcover_vacvge.jpg"} alt="mockups of a website done for Northwestern Feinberg's Student Health Leaders program."/>
                 <p className={rightStyles.projectName}><a href="./shl">Student Health Leaders (service design + ui/ux) → </a></p>
             </div>
             <div className={rightStyles.divider}/>
