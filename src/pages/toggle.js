da7f0bf977041bde46627b7c46240fc074bb0e9b
Stephanie Zhu <jiaruizhu2022@u.northwestern.edu>
2025-04-17 23:35:30 -0700

    did i fix the bug

--- src/pages/toggle.js
+++ src/pages/toggle.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MoonBlack from "../images/icons/moon-black.svg";
 import MoonWhite from "../images/icons/moon-white.png";
 import SunBlack from "../images/icons/sun-black.svg";
@@ -8,42 +8,35 @@ import LogoWhite from "../images/logo-white.png";
 import "../styles/styles.less";
 
 function ToggleButton() {
-  const [toggleView, setToggleView] = useState("day");
+  const [toggleView, setToggleView] = useState(() =>
+    typeof window !== "undefined" ? localStorage.getItem("mode") : null
+  );
 
-  /*  function ToggleMode() {
-    if (toggleView === "day") {
-      setToggleView("night");
-      document.body.style.backgroundColor = "black";
-      document.body.style.color = "white";
-      document
-        .querySelectorAll("a")
-        .forEach((link) => (link.style.color = "white"));
-      document
-        .querySelectorAll(".bm-burger-bars")
-        .forEach((bar) => (bar.style.background = "white"));
-      document.getElementById("navbar-logo").src = LogoWhite;
-      localStorage.setItem("mode", "night");
-    } else if (toggleView === "night") {
-      setToggleView("day");
-      document.body.style.backgroundColor = "white";
-      document.body.style.color = "black";
-      document
-        .querySelectorAll("a")
-        .forEach((link) => (link.style.color = "black"));
-      document.getElementById("navbar-logo").src = Logo;
-      document
-        .querySelectorAll(".bm-burger-bars")
-        .forEach((bar) => (bar.style.background = "black"));
-      localStorage.setItem("mode", "day");
-    }
+  function ToggleMode() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    document
+      .querySelectorAll("a")
+      .forEach((link) => (link.style.color = "black"));
+    document
+      .querySelectorAll(".bm-burger-bars")
+      .forEach((bar) => (bar.style.background = "black"));
+    document.getElementById("navbar-logo").src = Logo;
+    localStorage.setItem("mode", "day");
   }
- */
+
+  useEffect(() => {
+    window.addEventListener("load", ToggleMode);
+    return () => window.removeEventListener("load", ToggleMode);
+  });
+
   return (
     <div
       className="toggle"
       onClick={() =>
         toggleView === "day"
           ? (setToggleView("night"),
+            localStorage.setItem("mode", "night"),
             (document.body.style.backgroundColor = "black"),
             (document.body.style.color = "white"),
             document
@@ -52,9 +45,9 @@ function ToggleButton() {
             document
               .querySelectorAll(".bm-burger-bars")
               .forEach((bar) => (bar.style.background = "white")),
-            (document.getElementById("navbar-logo").src = LogoWhite),
-            localStorage.setItem("mode", "night"))
+            (document.getElementById("navbar-logo").src = LogoWhite))
           : (setToggleView("day"),
+            localStorage.setItem("mode", "day"),
             (document.body.style.backgroundColor = "white"),
             (document.body.style.color = "black"),
             document
@@ -63,8 +56,7 @@ function ToggleButton() {
             (document.getElementById("navbar-logo").src = Logo),
             document
               .querySelectorAll(".bm-burger-bars")
-              .forEach((bar) => (bar.style.background = "black")),
-            localStorage.setItem("mode", "day"))
+              .forEach((bar) => (bar.style.background = "black")))
       }
     >
       <div
