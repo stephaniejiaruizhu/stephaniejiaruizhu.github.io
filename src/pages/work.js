cd904919e4cd653a0ac9e79c29981a3994612c8b
Stephanie Zhu <jiaruizhu2022@u.northwestern.edu>
2025-04-30 16:58:27 -0700

    change link for top 100

--- src/pages/work.js
+++ src/pages/work.js
@@ -33,7 +33,7 @@ function Work() {
           <p className="project-date">03.31.25</p>
           <p className="project-title">
             <a
-              href="https://www.sfchronicle.com/projects/2025/top-100-best-restaurants-san-francisco-bay-area/?utm_source=marketing&utm_medium=copy-url-link&utm_campaign=article-share&hash=aHR0cHM6Ly93d3cuc2ZjaHJvbmljbGUuY29tL3Byb2plY3RzLzIwMjUvdG9wLTEwMC1iZXN0LXJlc3RhdXJhbnRzLXNhbi1mcmFuY2lzY28tYmF5LWFyZWEv&time=MTc0NDY3Njc3MzY5OQ==&rid=MTcwNzU5Y2MtMDczZi00ZWE1LTlmZjEtYjI4NTJhNmJjNWFj&sharecount=MQ=="
+              href="https://www.sfchronicle.com/projects/2025/top-100-best-restaurants-san-francisco-bay-area/"
               rel="noreferrer"
               target="_blank"
               className="project-link"
