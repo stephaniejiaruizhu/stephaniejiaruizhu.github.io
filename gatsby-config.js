94cb5bdfcf20175af54a9ad84301a79557674c67
Stephanie Zhu <jiaruizhu2022@u.northwestern.edu>
2026-01-12 14:21:31 -0800

    added dev ssr

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  flags: {
+    DEV_SSR: true,
+  },
   siteMetadata: {
     title: `stephaniejiaruizhu`,
     siteUrl: `https://www.stephaniejiaruizhu.com`,
   },
   plugins: [`gatsby-plugin-less`],
-  
-}
+};
