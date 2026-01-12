0e4ccdb8453b9916b288a740ee791ad140b9b24a
Stephanie Zhu <jiaruizhu2022@u.northwestern.edu>
2026-01-12 14:56:34 -0800

    Updates

--- component---src-pages-footer-js-5e4cad968a01a867bec1.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkstephaniejiaruizhu=self.webpackChunkstephaniejiaruizhu||[]).push([[146],{5688:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=n(7294),a=n(7606),r=n(3168);var i=function(){return u.createElement(u.Fragment,null,u.createElement("p",{className:"footer-module--footer--m9gMX"}," ","Built with ",u.createElement(a.G,{icon:r.m6i})," in React"))}}}]);
-//# sourceMappingURL=component---src-pages-footer-js-5e4cad968a01a867bec1.js.map
