8aefe168ffd06982d003c8ba4e7570117263d631
Stephanie Zhu <jiaruizhu2022@u.northwestern.edu>
2026-01-13 13:02:03 -0800

    hopefully fix popup

--- src/pages/popup.js
+++ src/pages/popup.js
@@ -327,13 +327,13 @@ function Popup({ info, card, cardscroll, popup, mode, setPopup }) {
           style={{ color: mode === "day" ? dark : light }}
           className="popup-title"
         >
-          {info.title}
+          {info ? info.title : null}
         </h3>
         <p
           style={{ color: mode === "day" ? neonpink : neongreen }}
           className="popup-author"
         >
-          {info.author ? info.author : null}
+          {info ? info.author : null}
         </p>
         {info.image != null ? (
           <img className="popup-image" src={info.image} alt="book cover" />
